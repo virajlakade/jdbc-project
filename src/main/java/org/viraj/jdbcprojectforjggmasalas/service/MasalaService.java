d8486143542804d15e6f76c4790d3bd0e0a4b002
virajlakade <lakadeviraj09@gmail.com>
2026-02-26 12:22:25 +0530

    Sevice method Almost Done

--- src/main/java/org/viraj/jdbcprojectforjggmasalas/service/MasalaService.java
+++ src/main/java/org/viraj/jdbcprojectforjggmasalas/service/MasalaService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import org.viraj.jdbcprojectforjggmasalas.model.Masala;
 import org.viraj.jdbcprojectforjggmasalas.repo.MasalaRepo;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MasalaService {
     @Autowired
@@ -12,5 +15,19 @@ public class MasalaService {
     public Masala addMasala(Masala masala){
         return repo.save(masala);
     }
+    public List<Masala> getallmasala(){
+        return repo.findAll();
+    }
+    public Masala getMasalabyId(Long id){
+        Optional<Masala> masala=repo.findById(id);
+        return masala.orElse(null);
+    }
+    public void deleteStudent(Long id){
+        repo.deleteById(id);
+    }
+    public Masala updateMasala(Masala masala){
+        return repo.save(masala);
+    }
+
 
 }
